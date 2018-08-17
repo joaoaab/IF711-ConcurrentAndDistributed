fbaf4c85896a5db45505d9007dec5dd9c7f528cd
João Assunção <jaab@cin.ufpe.br>
2018-08-17 12:20:26 -0300

    Adicionei comentários

--- Exercicio1/semdeadlock.cpp
+++ Exercicio1/semdeadlock.cpp
@@ -6,19 +6,24 @@
 #include <string>
 #include <sstream>
 
+// Variáveis que serão manipuladas pelas threads
 int custo_bom = 0;
 int indice = 0;
 int custo_ruim = 0;
 
+// Mutexes
 std::mutex xflags,xbom;
 std::mutex mutex_array[100];
 
 
 void contador(std::vector<int>& flag, std::vector<int>& custo_ruim ,int num_arch){
     int f,q;
+    // Entrando em região crítica
     xflags.lock();
     std::ifstream input;
     while(indice < num_arch){
+        // Achar um arquivo que nenhuma thread tenha usado
+        // e aí abre o arquivo
         while(!input.is_open()){
             std::cout << "Não aberto, thread :" << std::this_thread::get_id() << std::endl;
             if(!flag[indice]){
@@ -27,9 +32,11 @@ void contador(std::vector<int>& flag, std::vector<int>& custo_ruim ,int num_arch
             }
             indice++;
         }
+        // Saíndo de região crítica
         std::cout << "aberto, indice :  " << indice <<  ", thread :" << std::this_thread::get_id() << " tentando travar" << std::endl;
         xflags.unlock();    
-        std::cout << "travado, calculando" << std::this_thread::get_id() << std::endl;
+        // Calculando custos a partir do arquivo
+        // Duas regiões críticas
         while(input >> f >> q){
             if(q == 0){
                 custo_bom += q;
@@ -45,6 +52,7 @@ void contador(std::vector<int>& flag, std::vector<int>& custo_ruim ,int num_arch
                 mutex_array[f].unlock();
             }
         }
+
         xflags.lock();
         if(input.is_open()){
             input.close();
@@ -54,20 +62,26 @@ void contador(std::vector<int>& flag, std::vector<int>& custo_ruim ,int num_arch
 }
 
 int main(){
+    // Variáveis de quantidade
     int num_threads, flechas, i, f, q, rc, num_arch;
+    // Lendo arquivo inicial das constantes
     std::string entrada;
     std::ifstream input("entrada.in");
     input >> num_arch >> num_threads >> flechas;
+    // Criando estruturas para cálculo dos preços
     std::vector<int> preco(flechas, 0);
     std::vector<int> custo_ruim(flechas, 0);
     std::vector<std::thread> threads;
     std::vector<int> flag(num_arch , 0);
+    // Lendo arquivo de preços
     for(int i = 0; i < flechas ; i++){
         input >> preco[i];
     }
+    // Criando threads
     for(int i = 0; i < num_threads; i++){
         threads.push_back(std::thread(contador,std::ref(flag),std::ref(custo_ruim),num_arch));
     }
+    // Esperando que terminem
     for(auto& th : threads){
         th.join();
     }
