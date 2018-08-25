0ace5bda7e13de0579c16613627a522faaf63df8
Jailson Gomes <jjgsj@cin.ufpe.br>
2018-08-25 16:53:40 +0100

    apenas clients

---
+++ Exercicio2/client_udp.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	//Connect TCP
+	conn, err := net.Dial("udp", "localhost:1313")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	//simple Read
+	buffer := make([]byte, 1024)
+	conn.Read(buffer)
+
+	//simple write
+	conn.Write([]byte("Hello from client"))
+}
