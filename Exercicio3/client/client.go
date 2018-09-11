4252abdb1ed6ef950dafba378a6f4efaa08020c9
Rodrigo César <rcdl@cin.ufpe.br>
2018-09-10 22:12:20 -0300

    Receive as a function

--- Exercicio3/client/client.go
+++ Exercicio3/client/client.go
@@ -31,30 +31,7 @@ func sendMessage(ch *amqp.Channel) {
 	}
 }
 
-func receiveMessage() {
-
-}
-
-func main() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
-
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
-
-	err = ch.ExchangeDeclare(
-		"chatglobal", // name
-		"fanout",     // type
-		true,         // durable
-		false,        // auto-deleted
-		false,        // internal
-		false,        // no-wait
-		nil,          // arguments
-	)
-	failOnError(err, "Failed to declare an exchange")
-
+func receiveMessage(ch *amqp.Channel) {
 	q, err := ch.QueueDeclare(
 		"",    // name
 		false, // durable
@@ -84,15 +61,35 @@ func main() {
 	)
 	failOnError(err, "Failed to register a consumer")
 
+	for d := range msgs {
+		log.Printf(" [x] %s", d.Body)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		"chatglobal", // name
+		"fanout",     // type
+		true,         // durable
+		false,        // auto-deleted
+		false,        // internal
+		false,        // no-wait
+		nil,          // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+
 	forever := make(chan bool)
 
 	go sendMessage(ch)
-
-	go func() {
-		for d := range msgs {
-			log.Printf(" [x] %s", d.Body)
-		}
-	}()
+	go receiveMessage(ch)
 
 	log.Printf(" [*] Connected. To exit press CTRL+C")
 	<-forever
