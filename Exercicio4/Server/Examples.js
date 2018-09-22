f8076b3d38d6e202a1b212d23f96e442d70e0efa
João Assunção <jaab@cin.ufpe.br>
2018-09-21 21:23:47 -0300

    Going to use Nodejs as server and golang as client

---
+++ Exercicio4/Server/Examples.js
@@ -0,0 +1,60 @@
+// how to create a tcp server
+
+var net = require('net');
+
+var server = net.createServer(function(socket) {
+	socket.write('Echo server\r\n');
+	socket.pipe(socket);
+});
+
+server.listen(1337, '127.0.0.1');
+
+var client = new net.Socket();
+client.connect(1337, '127.0.0.1', function() {
+	console.log('Connected');
+	client.write('Hello, server! Love, Client.');
+});
+
+client.on('data', function(data) {
+	console.log('Received: ' + data);
+	client.destroy(); // kill client after server's response
+});
+
+client.on('close', function() {
+	console.log('Connection closed');
+});
+
+// UDP Server
+
+var PORT = 33333;
+var HOST = '127.0.0.1';
+
+var dgram = require('dgram');
+var server = dgram.createSocket('udp4');
+
+server.on('listening', function () {
+    var address = server.address();
+    console.log('UDP Server listening on ' + address.address + ":" + address.port);
+});
+
+server.on('message', function (message, remote) {
+    console.log(remote.address + ':' + remote.port +' - ' + message);
+
+});
+
+server.bind(PORT, HOST);
+
+
+// UDP client
+var PORT = 33333;
+var HOST = '127.0.0.1';
+
+var dgram = require('dgram');
+var message = new Buffer('My KungFu is Good!');
+
+var client = dgram.createSocket('udp4');
+client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+    if (err) throw err;
+    console.log('UDP message sent to ' + HOST +':'+ PORT);
+    client.close();
+});
